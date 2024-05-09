cdc65108432a96b2022401aeba145ba5e91d37f6
YaredMordechai <Yaredmordechai@gmail.com>
2024-05-09 08:23:32 +0700

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Desti from "./destination";
 
-const root = ReactDOM.createRoot(document.getElementById('roott'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Desti />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
