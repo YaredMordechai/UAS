45fe89e5a129d6f54d43904e9959c88b55444b65
vedianbnka <v.bunka238@gmail.com>
2024-05-15 17:03:54 +0700

    edit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <section className="atas1">
-      <h1>Oh. So. Wonderful.</h1>
+      <h2 class="typewrite text-white" data-period="2000" data-type='[ "Hello, World!.","Selamat Datang Di Bisabos","Temukan dan Baca Artikel Seputar Koding Ada Disini." ]'>
+            <span class="wrap">Hello, World!.</span>
+        </h2>
     </section>
   );
 }
