64de21b96e4f711236a90ca25809ef4924c849eb
vedianbnka <v.bunka238@gmail.com>
2024-05-17 10:00:12 +0700

    revisi

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
       <h2
         class="typewrite text-white"
         data-period="500"
-        data-type='[ "Sumatera Barat","Oh. So. Wonderful","What. A. Beauty" ]'
+        data-type='[ "Sumatera Barat","Oh. So. Wonderful.","What. A. Beauty" ]'
       >
         <span class="wrap">Sumatera Barat</span>
       </h2>
--- my-app/src/footer.jsx
+++ my-app/src/footer.jsx
@@ -8,49 +8,16 @@ function futer() {
         <div className="container">
           <div className="row">
             <div className="col-sm-12 col-md-6">
-              <h6>About</h6>
+              <h6>Sumatera Barat</h6>
               <p className="text-justify">
-                Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative
-                to help the upcoming programmers with the code. Scanfcode
-                focuses on providing the most efficient code or snippets as the
-                code wants to be simple. We will help programmers build up
-                concepts in different programming languages that include C, C++,
-                Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and
-                Algorithm.
+                Sumatera Barat, sebuah provinsi yang berada di ujung bawah pulau
+                Sumatera yang kaya akan keindahan alam, kekayaan budaya, cita
+                rasa kuliner, dan juga keramahan penduduk yang menakjubkan yang
+                menunggu untuk dijelajahi. Temukan pesonanya bersama kami.
               </p>
             </div>
 
-            <div className="col-xs-6 col-md-3">
-              <h6>Categories</h6>
-              <ul className="footer-links">
-                <li>
-                  <a href="http://scanfcode.com/category/c-language/">C</a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/front-end-development/">
-                    UI Design
-                  </a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/back-end-development/">
-                    PHP
-                  </a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/java-programming-language/">
-                    Java
-                  </a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/android/">Android</a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/templates/">
-                    Templates
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <div className="col-xs-6 col-md-3"></div>
 
             <div className="col-xs-6 col-md-3">
               <h6>Quick Links</h6>
